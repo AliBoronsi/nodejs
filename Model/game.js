0aa30442689475f2f7ea633334fd036a2c498587
ali <boronsi1990@gmail.com>
2019-02-16 13:42:43 +0330

    1

---
+++ Model/game.js
@@ -0,0 +1,14 @@
+export default class {
+    constructor(id, p1, g1, p2, g2) {
+        this.id = id;
+        this.p1 = p1;
+        this.g1 = g1;
+        this.p2 = p2;
+        this.g2 = g2;
+    }
+    id = 0;
+    p1 = '';
+    g1 = 0;
+    p2 = '';
+    g2 = 0;
+}
