fe00e99dcf746a89e39f564f3c2f4b2a1f500b06
ali <boronsi1990@gmail.com>
2019-02-17 10:34:16 +0330

    1

--- app.js
+++ app.js
@@ -14,7 +14,8 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-app.use(Utility.RedirectToHTTPS);
+
+app.get('*',Utility.RedirectToHTTPS);
 
 app.use('/', indexRouter);
 app.use('/games', gamesRouter);
